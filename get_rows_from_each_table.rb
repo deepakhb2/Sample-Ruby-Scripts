0e3a3dcacac9871e46394e470d286ec1f80a9e8a
Paladion <paladion@paladion-ThinkPad-Edge-E430.(none)>
2013-09-03 15:36:54 +0530

    Getting number of records in each table in the mentioned database

---
+++ get_rows_from_each_table.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'dbi'
+
+db_handler = DBI.connect('DBI:Mysql:provian_latest_db:localhost', 'root', 'root')
+
+# Collect all Tables
+sql_1 = db_handler.prepare('SHOW tables;')
+sql_1.execute
+tables = sql_1.map { |row| row[0]}
+sql_1.finish
+write_file = ''
+tables.each do |table_name|
+  sql_2 = db_handler.prepare("SELECT count(*) FROM #{table_name};")
+  sql_2.execute
+  sql_2.each do |row|
+    write_file = write_file+"Table '#{table_name}' has '#{row[0]}' rows.\n"
+  end
+  sql_2.finish
+end
+db_handler.disconnect
+File.open("db_rows_count.txt", 'w') { |file| file.write(write_file) }
